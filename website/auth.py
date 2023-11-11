2f08d6e6dd6646d23335329f42bed62064c2186c
Matheus <matheusdiaslandim@gmail.com>
2023-11-11 13:42:14 -0300

    Login Module

--- website/auth.py
+++ website/auth.py
@@ -22,13 +22,13 @@ def login():
         else:
             flash('Email does not exits.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("login.html"))
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -57,4 +57,4 @@ def sign_up():
             
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html",user=current_user)
