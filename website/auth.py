c4bb804b498907cac8cad4f1f7f6b18dc36dff85
Matheus <matheusdiaslandim@gmail.com>
2023-11-16 11:44:08 -0300

    .

--- website/auth.py
+++ website/auth.py
@@ -6,27 +6,6 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-"""
-@auth.route('/login', methods=['GET','POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Logged in successfully!', category='success')
-            login_user(user, remember=True)
-        else:
-            flash('Email does not exits.', category='error')
-
-    return render_template("login.html", user=current_user)
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
-    
-"""
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
     if request.method == 'POST':
